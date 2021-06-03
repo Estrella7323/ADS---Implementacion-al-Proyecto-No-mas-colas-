05b69bf8048521318622cb74faa28bfcb6c19150
GiancarloQuispeTaco <82392276+GiancarloQuispeTaco@users.noreply.github.com>
2021-06-03 14:04:26 -0500

    Modificacion de la clase Domicilio

--- src/Domicilio.java
+++ src/Domicilio.java
@@ -6,44 +6,38 @@ public class Domicilio {
 	private String calle;
 	private String numero;
 	private String distrito;
-	public Domicilio() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Ingrese calle , numero y distrito:");
-		this.calle = sc.next();
-		this.numero = sc.next();
-		this.distrito = sc.next();
-	}
 	
-	public Domicilio(String calle, String numero , String distrito) {
-		this.setCalle(calle);
-		this.setDistrito(distrito);
-		this.setNumero(numero);
-		
-		
-	}
-
-	public String getNumero() {
-		return numero;
-	}
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-	
-	public String getDistrito() {
-		return distrito;
-	}
-	public void setDistrito(String distrito) {
-		this.distrito = distrito;
-	}
-	
-	public String getCalle() {
-		return calle;
-	}
-	public void setCalle(String calle) {
+	public Domicilio(String calle, String numero, String distrito) {
 		this.calle = calle;
-	}
-	public String toString() {
-		return this.calle + " " + this.numero + " - " + this.distrito;
-	}
-	
+		this.numero = numero;
+		this.distrito = distrito;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getDistrito() {
+        return distrito;
+    }
+    public void setDistrito(String distrito) {
+        this.distrito = distrito;
+    }
+
+    public String toString() {
+        return (this.calle + " "+ this.numero +" "+ this.distrito);
+    }
+
+
+
 }
