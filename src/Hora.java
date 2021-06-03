549578b920738252e671da53d1034097a5276d6c
gustavovizcarrabenavides <77975543+gustavovizcarrabenavides@users.noreply.github.com>
2021-06-03 17:21:33 -0500

    Update Hora.java
    "Implementación de la clase Hora. TIene el fin de colocar una hora para pedir una cita de parte del paciente "

--- src/Hora.java
+++ src/Hora.java
@@ -1,4 +1,42 @@
+import java.util.Scanner;
 
 public class Hora {
+
+    private String minutos;
+    private String horas;
+    public Hora() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la hora ( hora minutos(");
+        this.minutos = sc.next();
+        this.horas = sc.next();
+    }
+
+    public Hora(String horas, String minutos ) {
+        this.setHoras(horas);
+        this.setMinutos(minutos);
+
+    }
+
+    public String getHoras() {
+        return horas;
+    }
+
+    public void setHoras(String horas) {
+        this.horas = horas;
+    }
+
+    public String getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(String minutos) {
+        this.minutos = minutos;
+    }
+
+    public String toString() {
+        return "Hora: " + this.horas + ":" + this.minutos;
+    }
+
+
 
 }
