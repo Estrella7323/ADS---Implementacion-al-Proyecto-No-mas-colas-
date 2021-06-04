491a8812378b2e730690e2c50bba7ba9593c7956
luis-angelmh <68036225+luis-angelmh@users.noreply.github.com>
2021-06-03 22:27:08 -0500

    Modificando e Implementando clase Paciente
    Mejorando el método ToString para que la salida se vea ordenada.

--- src/Paciente.java
+++ src/Paciente.java
@@ -1,65 +1,70 @@
-
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 public class Paciente  extends Person implements Comparable<Paciente> {
-	
-	private int id;
-	private Domicilio direccion;
-	private Hora Horallegada;
-	private Calendar fechaAtencion;
-	
-	public Paciente(int id, String nombre,String apellido,int edad ,int dni , Domicilio direccion ,Hora Horallegada,Calendar fechaAtencion) {
-		super(nombre , apellido ,edad ,dni);
-		this.direccion = new Domicilio();
-		this.Horallegada = new Hora ();
-		this.fechaAtencion = fechaAtencion;
-		
-	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
+
+    private int id;
+    private Domicilio direccion;
+    private Hora horallegada;
+    private Calendar fechaAtencion;
 
 
 
-	public Domicilio getDireccion() {
-		return direccion;
-	}
-	public void setDireccion(Domicilio direccion) {
-		this.direccion = direccion;
-	}
-	public Calendar getFechaAtencion() {
-		return fechaAtencion;
-	}
-	public void setFechaAtencion(Calendar fechaAtencion) {
-		this.fechaAtencion = fechaAtencion;
-	}
+    public Paciente(int id, String nombre,String apellido,int edad ,int dni , Domicilio direccion ,Hora horallegada,Calendar fechaAtencion) {
+        super(nombre , apellido ,edad ,dni);
+        this.id = id;
+        this.direccion = direccion;
+        this.horallegada = horallegada;
+        this.fechaAtencion = fechaAtencion;
 
-	public Hora getHorallegada() {
-		return Horallegada;
-	}
+    }
 
-	public void setHorallegada(Hora horallegada) {
-		Horallegada = horallegada;
-	}
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 
 
 
-	@Override
-	public String toString() {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-		return "Paciente : [id=" + id + super.toString() + 
-				", Direccion=" + this.direccion + "Hora de llegada\t; " + this.Horallegada+"\n"+
-				"fechaAtencion=" + sdf.format(this.fechaAtencion.getTime()) + "]";
-	}
+    public Domicilio getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(Domicilio direccion) {
+        this.direccion = direccion;
+    }
+    public Calendar getFechaAtencion() {
+        return fechaAtencion;
+    }
+    public void setFechaAtencion(Calendar fechaAtencion) {
+        this.fechaAtencion = fechaAtencion;
+    }
 
-	@Override
-	public int compareTo(Paciente o) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public Hora getHorallegada() {
+        return horallegada;
+    }
+
+    public void setHorallegada(Hora horallegada) {
+        this.horallegada = horallegada;
+    }
+
+
+
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        return "Paciente:\n"+
+                "id:"+ id + "\n"+
+                super.toString() +"\n"+
+                "Direccion:" + this.direccion +"\n"+
+                "Hora de Atencion: " + this.horallegada+"\n"+
+                "Fecha de Atencion=" + sdf.format(this.fechaAtencion.getTime()) + "\n";
+    }
+
+    @Override
+    public int compareTo(Paciente o) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
 }
