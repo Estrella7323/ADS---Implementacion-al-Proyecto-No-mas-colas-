ae5209658c568234b963a58baa16063b268ed031
Estrella7323 <73238465@ucsm.edu.pe>
2021-07-14 23:43:53 -0500

    Creacion de clase Test para probar  funciones
    Se probaran funciones de Grafos , en especial  la busqueda del camino mas corto  implementando el algoritmo Dijkstra

--- src/Test.java
+++ src/Test.java
@@ -1,27 +1,24 @@
+package grafos;
+public class Test 
+{
+    public static void main(String[] args) 
+    {
+        GraphLink<Integer> g2=new GraphLink<Integer>();
+        g2.insertVertex(1);
+        g2.insertVertex(2);
+        g2.insertVertex(3);
+        g2.insertVertex(7);
+        g2.insertVertex(8);
+        g2.insertVertex(6);
+        g2.insertEdge(2,6);
+        g2.insertEdge(7,8);
+        g2.insertEdge(1,3);
+        g2.insertEdge(1,2);
+        g2.insertEdge(3,7);
+        g2.insertEdge(2,7);
+        g2.insertEdge(2,6);
+        System.out.println(g2);
+        g2.BFS(1);
+    }
 
-public class Test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Queue<String> q = new ArrayQueue<String>(2);
-		try {
-		q.enqueue("eeeeeeeeeee");
-		q.enqueue("aaaaaaaaa");
-		q.enqueue("iiiiii");
-		q.enqueue("uuuuuu");
-		q.enqueue("eeeeee");
-		q.enqueue("adasdad");
-		q.enqueue("gsdgsg");
-		q.enqueue("sdfsdgsdg");
-		q.enqueue("aaaaaaaaaaaaaaaaaaaaaaa");
-		
-		while(!q.isEmpty())
-			System.out.println("Elemento :"+ q.dequeue());
-	}
-	catch(ExceptionIsEmpty x) {
-		System.out.println(x.getMessage());
-		
-	}
-	}
-		
 }
