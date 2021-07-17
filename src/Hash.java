1f175050bcf354f126c58472059e1a7b2f9099b3
gustavovizcarrabenavides <77975543+gustavovizcarrabenavides@users.noreply.github.com>
2021-07-16 21:25:09 -0500

    Para evitar colisiones se hizo Hashing por método lineal. Además se insertó el método insert para agregarlo a nuestro HashTable

--- src/Hash.java
+++ src/Hash.java
@@ -26,4 +26,39 @@ public class Hash<E extends Comparable<E>> {
     private int functionHash(int key) {
         return key % m;
     }
+    
+        private int linearProbing(int buildHash, int key) {
+
+        int posInit = buildHash;
+
+        do {
+            if(table.get(buildHash).marca == 0)
+                return buildHash;		//se encontro posicion vacia
+
+            if(table.get(buildHash).marca    == 1 && table.get(buildHash).reg.getKey() == key)
+                return -2;
+
+            buildHash = (buildHash + 1) % m; // para 1
+
+        } while(buildHash != posInit);
+
+        return -1;
+    }
+
+    public void insert(int key, String reg) {
+        int buildHash = functionHash(key); // 0
+        buildHash = linearProbing(buildHash, key);
+        if (buildHash == -1) {
+            System.out.println("Key table hash is full...");
+            return;
+        }
+        else if(buildHash == -2) {
+            System.out.println("key is duplicated");
+            return;
+        }
+        else {
+            Element aux = new Element(1,new RegisterHash<E>(key, (E) reg));
+            table.set(buildHash, aux);
+        }
+    }
 }    
